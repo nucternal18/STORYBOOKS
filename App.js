373462f4b8e1a252ef68ca493927b17a1c4a558f
nucternal18 <aolausoro@gmail.com>
2020-06-21 13:50:29 +0100

    created dashboar views, routes and implemented google oauth20 login authentication

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path')
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
-const sessions = require('express-session')
+const sessions = require('express-session');
+const MongoStore = require('connect-mongo')(sessions);
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,8 +21,18 @@ connectDB();
 
 const app = express();
 
+//Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Handlebars Helpers
+const { formatDate } = require('./helpers/hbs');
+
 // Handlebars engine
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs({
+  helpers: {
+  formatDate
+}, defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -28,6 +40,7 @@ app.use(sessions({
   secret: "Bankai",
   resave: false,
   saveUninitialized: true,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
 }))
 // Passport Middleware
 app.use(passport.initialize())
@@ -39,6 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
